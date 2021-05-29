d9ef608bda652e96774885289ef9530ea4c730e3
extrodnare274 <76926053+extrodnare274@users.noreply.github.com>
2021-05-28 20:34:24 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+
+    stone1 = new Stone(600,600,50)
+    iron = new Iron(800,200,50,50)
+    rubber = new Rubber(400,400,23)
+
+    stone2 = new Stone(650,650,60)
+    iron2 = new Iron(850,250,60,60)
+    rubber2 = new Rubber(450,450,33)
+
+
+  
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    stone1.display();
+    rubber.display();
+    iron.display();
+    stone2.display();
+    rubber2.display();
+    iron2.display();
+    
+ 
+}
